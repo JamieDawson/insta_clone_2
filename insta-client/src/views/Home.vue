dcf801d1476627606372273caa7009beb505b448
Jamie Dawson <jadawson@e1z2r6p10.42.us.org>
2019-10-27 15:35:56 -0700

    vid 4 32:48. premade images displaying on main page

--- insta-client/src/views/Home.vue
+++ insta-client/src/views/Home.vue
@@ -1,16 +1,24 @@
 <template>
   <main class="feed">
+    <article class="post" v-for="post in feed" :key="post.id">
+      <header class="user-user">{{ post.display_name }}</header>
+      <section class="post-picture">
+        <img :src="post.image" :alt="post.desc" class="post-image" />
+      </section>
+      <footer class="post-desc">{{post.display_name + ': ' + post.desc}}</footer>
+    </article>
+
     <button @click="logout">Logout</button>
   </main>
 </template>
 
 <script>
 export default {
-  // computed: {
-  //   feed () {
-  //     return this.$store.state.feed;
-  //   }
-  // }
+  computed: {
+    feed() {
+      return this.$store.state.feed;
+    }
+  },
   methods: {
     logout() {
       this.$store.commit("logout");
