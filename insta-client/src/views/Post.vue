4efc725b5ce42047c4088eb99110c7fca2a8c766
Jamie Dawson <jadawson@e1z3r5p23.42.us.org>
2019-10-31 22:36:55 -0700

    vid5 11:10. Camera now works

--- insta-client/src/views/Post.vue
+++ insta-client/src/views/Post.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="post">
+  <main class="view post">
     <section class="stream">
       <video ref="video" id="video" width="100%" height="300" autoplay></video>
     </section>
@@ -14,7 +14,25 @@ export default {
     };
   },
   methods: {},
-  mounted() {}
+  mounted() {
+    this.video = this.$refs.video; //links to vide ref up above.
+    this.video.width = window.innerWidth;
+    this.video.height = window.innerHeight - 80;
+
+    if (navigator.mediaDevices && navigator.getUserMedia) {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true
+        })
+        .then(stream => {
+          this.video.srcObject = stream;
+          this.video.play();
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  }
 };
 </script>
 
